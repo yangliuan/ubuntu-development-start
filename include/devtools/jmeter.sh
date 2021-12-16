c1cb31091b67edc2fb2c99fe32a52ac43bac9ec0
yangliuancn <yangliuan@foxmail.com>
2021-12-16 23:12:42 +0800

    fix

--- include/devtools/jmeter.sh
+++ include/devtools/jmeter.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 Install_Jmeter(){
+    pushd ${oneinstack_dir}/src > /dev/null
+
+    echo "Download jmeter ..."
+    src_url="https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-${jmeter_ver}.zip" && Download_src
+    unzip apache-jmeter-${jmeter_ver}.zip
     
 }
