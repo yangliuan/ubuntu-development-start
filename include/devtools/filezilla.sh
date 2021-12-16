35ede86595cebba6ca5cbfa13442a1f0a2d12aed
yangliuancn <yangliuan@foxmail.com>
2021-12-16 23:19:39 +0800

    add

--- include/devtools/filezilla.sh
+++ include/devtools/filezilla.sh
@@ -2,5 +2,5 @@
 Install_FileZilla(){
     pushd ${oneinstack_dir}/src > /dev/null
     echo "Download filezilla ftp client ..."
-    src_url="https://dl2.cdn.filezilla-project.org/client/FileZilla_3.57.0_x86_64-linux-gnu.tar.bz2?h=h0uJPV3og0sqFAmLl7LSvg&x=1639505245" && Download_src
+    src_url="https://download.filezilla.cn/client/linux/FileZilla_3.56.0_i686-linux-gnu.tar.bz2" && Download_src
 }
