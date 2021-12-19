05fee1e63195355e6ef6afd9fb5861eb9b8b584e
yangliuancn <yangliuan@foxmail.com>
2021-12-19 22:41:48 +0800

    add install vscode

--- include/devtools/vscode.sh
+++ include/devtools/vscode.sh
@@ -2,8 +2,10 @@
 Install_Vscode(){
     src_url=$(/usr/local/php/bin/php ./include/devtools/get_redirect_url.php)
     src_url="http://vscode.cdn.azure.cn/stable/${src_url#*stable/}"
+    file_name="code_${src_url#*code_}"
     pushd ${oneinstack_dir}/src > /dev/null
-    Download_src
-    echo "Download vscode ..."
+    echo "Download vscode ..." && Download_src
+    dpkg -i ${file_name}
+    rm -rfv ${file_name}
     popd > /dev/null
 }
