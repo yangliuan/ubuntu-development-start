68a3bbd4197c01e9d31493d96055ea976bb6e90b
yangliuancn <yangliuan@foxmail.com>
2021-12-26 20:23:13 +0800

    modify

--- include/devtools/runapi.sh
+++ include/devtools/runapi.sh
@@ -6,11 +6,12 @@ Install_Runapi(){
     unzip 0e73b106b92346fcd7809de0ee71f0ff\?showdoc\=.zip -d runapi
     rm -rfv 0e73b106b92346fcd7809de0ee71f0ff\?showdoc\=.zip
     mkdir /opt/runapi
+    chown -Rv ${run_user}.${run_group} runapi/runapi.AppImage
+    chmod -Rv 755 runapi/runapi.AppImage
     mv -v runapi/runapi.AppImage /opt/runapi/
     rm -rfv runapi
     cp -rfv ${oneinstack_dir}/icon/runapi.png /opt/runapi/
     cp -rfv ${oneinstack_dir}/desktop/runapi.desktop /usr/share/applications/
-    chown -Rv ${run_user}.${run_group} runapi/
-    chmod -Rv 777 runapi/*
+    chmod -Rv 777 /opt/runapi/runapi.png
     popd > /dev/null
 }
