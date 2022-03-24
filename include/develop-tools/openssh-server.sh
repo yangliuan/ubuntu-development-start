968f5adff1a340c8e12dc26c5110f47e128b445e
yangliuan <yangliuancn@foxmail.com>
2022-03-24 14:24:33 +0800

    modify

---
+++ include/develop-tools/openssh-server.sh
@@ -0,0 +1,8 @@
+Install_OpensshServer(){
+    apt-get install openssh-server
+    ufw allow ssh
+    #修改配置文件允许root远程登录
+    sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+    service sshd restart
+    service sshd status
+}
