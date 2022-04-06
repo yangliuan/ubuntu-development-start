1250b9df2f45ad332c75a26c58b30a54b1b3116f
yangliuan <yangliuancn@foxmail.com>
2022-04-06 13:48:15 +0800

    modify

--- include/message-queue/kafka.sh
+++ include/message-queue/kafka.sh
@@ -9,8 +9,19 @@ Install_Kafka() {
     mv kafka_${kafka_ver}-${kafka_scala_ver} ${kafka_install_dir}
     sed -i "s#/tmp/zookeeper#${zookeeper_data_dir}#g" ${kafka_install_dir}
     
+    id -u kafka >/dev/null 2>&1
+    [ $? -ne 0 ] && useradd -M -s /sbin/nologin redis
+
+    cp zookeeper.service /lib/systemd/system/
+    cp kafka.service /lib/systemd/system/
+    systemctl daemon-reload
+    systemctl start zookeeper.service 
+    systemctl start kafka.service
 }
 
 Uninstall_Kafka() {
-    
+    rm -rf ${kafka_install_dir}
+    rm -rf /lib/systemd/system/zookeeper.service
+    rm -rf /lib/systemd/system/kafka.service
+    systemctl daemon-reload
 }
