b6449961284af85fa2274af2c3cdcc9b54a4ab5f
yangliuancn <yangliuan@foxmail.com>
2022-04-09 23:47:06 +0800

    modify

--- include/nodejs/switch_npm_registry.sh
+++ include/nodejs/switch_npm_registry.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 Switch_NpmRegistry() {
-    echo 1;
     #镜像数组
     npm_registry[0]="https://registry.npm.taobao.org"
     npm_registry[1]="http://www.npmjs.org"
@@ -11,9 +10,9 @@ Switch_NpmRegistry() {
     #获取输入的镜像序号
     echo
     read -e -p "Please input a number :" registry_option
-    if [[ ! ${registry_option} =~ ^[0-9]$|^6$ ]]; then
+    if [[ ! ${registry_option} =~ ^[0-1]$ ]]; then
         echo "input error! Please only input number 0~${i}:"
     fi
-    
+
     npm config set registry ${npm_registry[${registry_option}]}
 }
