b427b3079f6e7cac01d80f49e14df5f613b91137
yangliuancn <yangliuan@foxmail.com>
2022-04-16 03:52:26 +0800

    modify

--- include/message-queue/kafka.sh
+++ include/message-queue/kafka.sh
@@ -5,21 +5,23 @@ Install_Kafka() {
     pushd ${oneinstack_dir}/src > /dev/null
 
     echo "Download kafka ..."
-    src_url="https://dlcdn.apache.org/kafka/${kafka_scala_ver}/kafka_${kafka_ver}-${kafka_scala_ver}.tgz" && Download_src
-    tar xzf kafka_${kafka_ver}-${kafka_scala_ver}.tgz
-    mv kafka_${kafka_ver}-${kafka_scala_ver} ${kafka_install_dir}
-    sed -i "s#/tmp/zookeeper#${zookeeper_data_dir}#g" ${kafka_install_dir}/config/zookeeper.properties
-    sed -i "s/#advertised.listeners=PLAINTEXT:\/\/your.host.name:9092/advertised.listeners=PLAINTEXT:\/\/127.0.0.1:9092/g" ${kafka_install_dir}/config/server.properties
-    sed -i "s#/tmp/kafka-logs#${kafka_data_dir}#g" ${kafka_install_dir}/config/server.properties
+    src_url="https://dlcdn.apache.org/kafka/${kafka_ver}/kafka_${kafka_scala_ver}-${kafka_ver}.tgz" && Download_src
+    tar xzf kafka_${kafka_scala_ver}-${kafka_ver}.tgz
+    sed -i "s#/tmp/zookeeper#${zookeeper_data_dir}#g" kafka_${kafka_scala_ver}-${kafka_ver}/config/zookeeper.properties
+    sed -i "s/#advertised.listeners=PLAINTEXT:\/\/your.host.name:9092/advertised.listeners=PLAINTEXT:\/\/127.0.0.1:9092/g" kafka_${kafka_scala_ver}-${kafka_ver}/config/server.properties
+    sed -i "s#/tmp/kafka-logs#${kafka_data_dir}#g" kafka_${kafka_scala_ver}-${kafka_ver}/config/server.properties
+    mv kafka_${kafka_scala_ver}-${kafka_ver} ${kafka_install_dir}
 
     id -u kafka >/dev/null 2>&1
     [ $? -ne 0 ] && useradd -M -s /sbin/nologin kafka
-
-    cp zookeeper.service /lib/systemd/system/
-    cp kafka.service /lib/systemd/system/
+    
+    cp ${oneinstack_dir}/init.d/zookeeper.service /lib/systemd/system/
+    cp ${oneinstack_dir}/init.d/kafka.service /lib/systemd/system/
     systemctl daemon-reload
     systemctl start zookeeper.service
     systemctl start kafka.service
+    
+    echo "${CSUCCESS}Kafka installed successfully! ${CEND}"
 }
 
 Uninstall_Kafka() {
