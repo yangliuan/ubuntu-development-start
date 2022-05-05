cdaba5c4b5b54fbf0cae378bed127397c7afd6cf
yangliuan <yangliuancn@foxmail.com>
2022-05-05 13:22:50 +0800

    md

--- include/python/supervisor.sh
+++ include/python/supervisor.sh
@@ -4,6 +4,7 @@ Install_Supervisor() {
     apt install supervisor
     cp -rfv ${oneinstack_dir}/desktop/supervisord.desktop /usr/share/applications
     systemctl disable supervisor.service
+    chmod -R 777 /etc/supervisor
 }
 
 Uninstall_Supervisor() {
