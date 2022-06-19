e29858074d8ce0188e93841ec7ed9fc8b9731720
yangliuancn <yangliuan@foxmail.com>
2022-06-19 14:40:17 +0800

    modify

--- start/mysql_start.sh
+++ start/mysql_start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:
 
-systemctl mysqld.service start
-systemctl mysqld.service status
+systemctl start mysqld.service
+systemctl status mysqld.service
