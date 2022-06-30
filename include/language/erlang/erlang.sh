e20641924aad16f04917c09fc6c2c413be3ffaeb
yangliuancn <yangliuan@foxmail.com>
2022-06-30 16:46:23 +0800

    add

--- include/language/erlang/erlang.sh
+++ include/language/erlang/erlang.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 Install_Erlang() {
+   pushd ${oneinstack_dir}/src > /dev/null
+   echo "Download erlang ..."
    src_url="https://github.com/erlang/otp/releases/download/OTP-${erlang_ver}/otp_src_${erlang_ver}.tar.gz" && Download_src
+   
 }
 
 Uninstall_Erlang() {
