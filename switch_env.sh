f8e986b254efd2a8421041f99397080653ec7fea
yangliuancn <yangliuan@foxmail.com>
2022-07-01 15:34:26 +0800

    modify

--- switch_env.sh
+++ switch_env.sh
@@ -14,58 +14,99 @@ pushd ${oneinstack_dir} > /dev/null
 . ./include/color.sh
 . ./include/get_char.sh
 
-while :; do echo
-    read -e -p "Do you want to switch php ? [y/n](n): " switch_php_flag
-    switch_php_flag=${switch_php_flag:-n}
-    if [[ ! ${switch_php_flag} =~ ^[y,n]$ ]]; then
-        echo "${CWARNING}input error! Please only input 'y' or 'n'${CEND}"
-    else
-        if [ "${switch_php_flag}" == 'y' ]; then
-            while :; do echo
-                read -e -p "Do you want to switch composer ? [y/n](n): " switch_composer_flag
-                switch_composer_flag=${switch_composer_flag:-n}
-                if [[ ! ${switch_composer_flag} =~ ^[y,n]$ ]]; then
-                    echo "${CWARNING}input error! Please only input 'y' or 'n'${CEND}"
-                else
-                    break;
-                fi
-            done
+Show_Help() {
+  echo "Usage: $0  command ...[parameters]....
+  --php                 switch php version
+  --composer            switch composer version
+  --composer_mirrors    switch composer mirrors
+  --npm_registry        switch npm registry
+  --nginx               switch nginx
+  "
+}
 
-            while :; do echo
-                read -e -p "Do you want to switch composer mirrors? [y/n](n): " switch_mirrors_flag
-                switch_mirrors_flag=${switch_mirrors_flag:-n}
-                if [[ ! ${switch_mirrors_flag} =~ ^[y,n]$ ]]; then
-                    echo "${CWARNING}input error! Please only input 'y' or 'n'${CEND}"
-                else
-                    break;
-                fi
-            done
+ARG_NUM=$#
+TEMP=`getopt -o hvV --long php,composer,composer_mirrors,npm,nginx -- "$@" 2>/dev/null`
+[ $? != 0 ] && echo "${CWARNING}ERROR: unknown argument! ${CEND}" && Show_Help && exit 1
+eval set -- "${TEMP}"
+while :; do
+  [ -z "$1" ] && break;
+  case "$1" in
+    --php)
+      switch_php_flag=y; shift 1
+      ;;
+    --composer)
+      switch_composer_flag=y; shift 1
+      ;;
+    --composer_mirrors)
+      switch_mirrors_flag=y; shift 1
+      ;;
+    --npm_registry)
+      switch_registry_flag=y; shift 1
+      ;;
+    --nginx)
+      switch_nginx_flag=y; shift 1
+      ;;
+    --)
+      shift
+      ;;
+    *)
+      echo "${CWARNING}ERROR: unknown argument! ${CEND}" && Show_Help && exit 1
+      ;;
+  esac
+done
+
+if [ ${ARG_NUM} == 0 ]; then
+    while :; do echo
+        read -e -p "Do you want to switch php ? [y/n](n): " switch_php_flag
+        switch_php_flag=${switch_php_flag:-n}
+        if [[ ! ${switch_php_flag} =~ ^[y,n]$ ]]; then
+            echo "${CWARNING}input error! Please only input 'y' or 'n'${CEND}"
+        else
+            if [ "${switch_php_flag}" == 'y' ]; then
+                while :; do echo
+                    read -e -p "Do you want to switch composer ? [y/n](n): " switch_composer_flag
+                    switch_composer_flag=${switch_composer_flag:-n}
+                    if [[ ! ${switch_composer_flag} =~ ^[y,n]$ ]]; then
+                        echo "${CWARNING}input error! Please only input 'y' or 'n'${CEND}"
+                    else
+                        break;
+                    fi
+                done
+
+                while :; do echo
+                    read -e -p "Do you want to switch composer mirrors? [y/n](n): " switch_mirrors_flag
+                    switch_mirrors_flag=${switch_mirrors_flag:-n}
+                    if [[ ! ${switch_mirrors_flag} =~ ^[y,n]$ ]]; then
+                        echo "${CWARNING}input error! Please only input 'y' or 'n'${CEND}"
+                    else
+                        break;
+                    fi
+                done
+            fi
+            break;
         fi
-        break;
-    fi
-done
+    done
 
+    while :; do echo
+        read -e -p "Do you want to switch npm registry? [y/n](n): " switch_registry_flag
+        switch_registry_flag=${switch_registry_flag:-n}
+        if [[ ! ${switch_registry_flag} =~ ^[y,n]$ ]]; then
+            echo "${CWARNING}input error! Please only input 'y' or 'n'${CEND}"
+        else
+            break;
+        fi
+    done
 
-
-while :; do echo
-    read -e -p "Do you want to switch npm registry? [y/n](n): " switch_registry_flag
-    switch_registry_flag=${switch_registry_flag:-n}
-    if [[ ! ${switch_registry_flag} =~ ^[y,n]$ ]]; then
-        echo "${CWARNING}input error! Please only input 'y' or 'n'${CEND}"
-    else
-        break;
-    fi
-done
-
-while :; do echo
-    read -e -p "Do you want to switch nginx? [y/n](n): " switch_nginx_flag
-    switch_nginx_flag=${switch_nginx_flag:-n}
-    if [[ ! ${switch_nginx_flag} =~ ^[y,n]$ ]]; then
-        echo "${CWARNING}input error! Please only input 'y' or 'n'${CEND}"
-    else
-        break;
-    fi
-done
+    while :; do echo
+        read -e -p "Do you want to switch nginx? [y/n](n): " switch_nginx_flag
+        switch_nginx_flag=${switch_nginx_flag:-n}
+        if [[ ! ${switch_nginx_flag} =~ ^[y,n]$ ]]; then
+            echo "${CWARNING}input error! Please only input 'y' or 'n'${CEND}"
+        else
+            break;
+        fi
+    done
+fi
 
 if [ "${switch_php_flag}" == 'y' ]; then
     . include/language/php/switch_php.sh
