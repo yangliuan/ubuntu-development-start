5753539e3eb804a36e35c9c4d07a35a7c08e499c
yangliuancn <yangliuan@foxmail.com>
2022-07-09 22:58:02 +0800

    modify

--- include/language/php/framework-init/webman.sh
+++ include/language/php/framework-init/webman.sh
@@ -2,4 +2,16 @@
 Install_Webman() {
     pushd $framework_dir
     composer create-project workerman/webman
+    pushd webman
+    composer require webman/console -vvv
+    composer require robmorgan/phinx -vvv
+    composer require webman/cors -vvv
+    composer require illuminate/events -vvv
+    composer require illuminate/pagination -vvv
+    composer require psr/container ^1.1.1 -vvv
+    composer require illuminate/redis -vvv
+    composer require symfony/cache -vvv
+    composer require php-di/php-di -vvv
+    composer require doctrine/annotations -vvv
+    composer require hyperf/aop-integration:^1.1 -vvv
 }
