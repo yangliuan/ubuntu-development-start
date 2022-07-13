1afaef2312246b28d7248da7c02d095c6649dbe8
yangliuancn <yangliuan@foxmail.com>
2022-07-13 13:24:35 +0800

    md

--- include/language/php/framework-init/webman.sh
+++ include/language/php/framework-init/webman.sh
@@ -4,14 +4,16 @@ Install_Webman() {
     pushd ${framework_dir} > /dev/null
     composer create-project workerman/webman
     pushd webman > /dev/null
+    sed -i "s@'controller_suffix' => ''@'controller_suffix' => 'Controller'@" ./config/app.php
+    cp -rfv ${oneinstack_dir}/include/language/php/framework-init/webman-config/database.php ./config/database.php
     composer require psr/container ^1.1.1 php-di/php-di doctrine/annotations -vvv
     composer require friendsofphp/php-cs-fixer --dev -vvv
-    cp -r ${oneinstack_dir}/include/language/php/framework-init/config/webman.php-cs-fixer.php ./.php-cs-fixer.php
+    cp -rfv ${oneinstack_dir}/include/language/php/framework-init/webman-config/webman.php-cs-fixer.php ./.php-cs-fixer.php
     composer require robmorgan/phinx -W -vvv
     composer require vlucas/phpdotenv -vvv
-    cp -r ${oneinstack_dir}/include/language/php/framework-init/config/env.webman ./.env
+    cp -rfv ${oneinstack_dir}/include/language/php/framework-init/webman-config/env.webman ./.env
     composer require webman/console -vvv
-    cp -r ${oneinstack_dir}/include/language/php/framework-init/config/phinx.php ./
+    cp -rfv ${oneinstack_dir}/include/language/php/framework-init/webman-config/phinx.php ./
     composer require webman/cors -vvv
     composer require illuminate/events -vvv
     composer require illuminate/pagination -vvv
