8d788780bf6123505b01ab29e947c5e5d9fc33aa
yangliuan <yangliuancn@foxmail.com>
2022-07-17 21:35:56 +0800

    md

--- include/language/nodejs/nvm.sh
+++ include/language/nodejs/nvm.sh
@@ -4,19 +4,21 @@ Install_Nvm() {
     pushd ${oneinstack_dir}/src > /dev/null
     src_url=http://mirror.yangliuan.cn/nvm-${nvm_ver}.tar.gz && Download_src
     tar -zxvf nvm-${nvm_ver}.tar.gz
-    mv -rfv nvm-${nvm_ver} ${nvm_install_dir}
+    mv -fv nvm-${nvm_ver} ${nvm_install_dir}
     #add env
-    cat >> /etc/profile.d/nvm.sh <<EOF
-export NVM_DIR="${nvm_install_dir}" ###nvm
+    cat >> /home/${run_user}/.bashrc <<EOF
+###nvm
+export NVM_DIR="\$HOME/.nvm" ###nvm
 [ -s "\$NVM_DIR/nvm.sh" ] && \. "\$NVM_DIR/nvm.sh" ###nvm
 [ -s "\$NVM_DIR/bash_completion" ] && \. "\$NVM_DIR/bash_completion" ###nvm
 EOF
-    source /etc/profile.d/nvm.sh
+    source /home/${run_user}/.bashrc
     popd > /dev/null
 }
 
 Uninstall_Nvm(){
     #delete env
+    sed -i '/##nvm$/d' /home/${run_user}/.bashrc
     rm -rfv ${nvm_install_dir} /etc/profile.d/nvm.sh
 }
 
