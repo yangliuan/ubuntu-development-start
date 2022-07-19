b2198cc6aa9c46d5f35092b05352555ef81aeba9
yangliuan <yangliuancn@foxmail.com>
2022-07-19 10:54:25 +0800

    md

--- include/language/go/go.sh
+++ include/language/go/go.sh
@@ -10,8 +10,17 @@ Install_Go() {
         rm -rf /usr/local/go
     fi
 
+    if [ ! -d "${go_path}" ]; then
+        mkdir ${go_path}
+    fi
+
     ln -s ${go_install_dir}${go_ver} ${go_install_dir}
-    sed -i "s@export PATH@export GOROOT="${go_install_dir}" #GOROOT 设置 ##Go\nexport GOPATH=$WORKSPACE/golang #GOPATH 设置 ##Go\nexport GO111MODULE=\"on\" #开启 Go moudles 特性 ##Go\nexport GOPROXY=https://goproxy.cn,direct #安装Go模块时，代理服务器设置 ##Go\nexport GOPRIVATE=  #指定不走代理的go包域名 ##Go\nexport GOSUMDB=off #关闭校验Go依赖包的哈希值 ##Go\nexport PATH@" /etc/profile
+    sed -i "s@export PATH@export GOROOT="${go_install_dir}" #GOROOT 设置 ##Go\nexport GOPATH=${go_path} #GOPATH 设置 ##Go\nexport GO111MODULE=\"on\" #开启 Go moudles 特性 ##Go\nexport GOPROXY=https://goproxy.cn,direct #安装Go模块时，代理服务器设置 ##Go\nexport GOPRIVATE=  #指定不走代理的go包域名 ##Go\nexport GOSUMDB=off #关闭校验Go依赖包的哈希值 ##Go\nexport PATH@" /etc/profile
+
+    [ -n "`grep ^'export PATH=' /etc/profile`" -a -z "`grep '$GOROOT/bin' /etc/profile`" ] && sed -i "s@^export PATH=\(.*\)@export PATH=\$GOROOT/bin:\1@" /etc/profile
+    [ -z "`grep ^'export PATH=' /etc/profile | grep '$GOROOT/bin'`" ] && echo 'export PATH=$GOROOT/bin:$PATH' >> /etc/profile
+    [ -n "`grep ^'export PATH=' /etc/profile`" -a -z "`grep '$GOPATH/bin' /etc/profile`" ] && sed -i "s@^export PATH=\(.*\)@export PATH=\$GOPATH/bin:\1@" /etc/profile
+    [ -z "`grep ^'export PATH=' /etc/profile | grep '$GOPATH/bin'`" ] && echo 'export PATH=$GOPATH/bin:$PATH' >> /etc/profile
     
     echo "install Go successed!"
 }
@@ -20,5 +29,6 @@ Uninstall_Go() {
     sed -i '/##Go$/d' /home/${run_user}/.bashrc
     source /home/${run_user}/.bashrc
     rm -rf /home/${run_user}/go/
+
     echo "uninstall Go successed!"
 }
