44cb31319750412e02b42fc4c40739d59302be33
yangliuan <yangliuancn@foxmail.com>
2022-07-21 12:01:39 +0800

    md

--- include/develop-tools/apifox.sh
+++ include/develop-tools/apifox.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 Install_Apifox() {
-    echo "install_apifox"
     pushd ${oneinstack_dir}/src > /dev/null
-    src_url='https://cdn.apifox.cn/download/Apifox-linux-deb-latest.zip' && Download_src
-    unzip Apifox-linux-deb-latest.zip
-    dpkg -i apifox_${apifox_ver}_amd64.deb
-    rm -rfv Apifox-linux-deb-latest.zip apifox_${apifox_ver}_amd64.deb
+    src_url='https://cdn.apifox.cn/download/Apifox-linux-manual-latest.tar.gz' && Download_src
+    tar -zxvf Apifox-linux-manual-latest.tar.gz
+    mv -fv apifox-${apifox_ver} /opt/apifox
+    chown -R ${run_user}.${run_user} /opt/apifox
+    chmod -R 775 /opt/apifox
+    rm -rfv apifox-${apifox_ver} Apifox-linux-manual-latest.tar.gz
     cp -rfv ${oneinstack_dir}/desktop/apifox.desktop /usr/share/applications/
+    
     popd > /dev/null
 }
 
 Uninstall_Apifox() {
-    dpkg -P apifox
+    rm -rf /opt/apifox /usr/share/applications/apifox.desktop
 }
 
