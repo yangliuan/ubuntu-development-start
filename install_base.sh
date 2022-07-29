3834cceec5038bb6f7529319bd23333dd8503792
yangliuancn <yangliuan@foxmail.com>
2022-07-29 18:54:48 +0800

    md

--- install_base.sh
+++ install_base.sh
@@ -746,16 +746,17 @@ if [ ${ARG_NUM} == 0 ]; then
       echo -e "\t${CMSG}12${CEND}. Install memcached"
       echo -e "\t${CMSG}13${CEND}. Install memcache"
       echo -e "\t${CMSG}14${CEND}. Install mongodb"
-      echo -e "\t${CMSG}15${CEND}. Install swoole"
-      echo -e "\t${CMSG}16${CEND}. Install event(PHP>=5.4)"
-      echo -e "\t${CMSG}17${CEND}. Install grpc(PHP>=7.0)"
-      echo -e "\t${CMSG}18${CEND}. Install xdebug(PHP>=5.5)"
-      echo -e "\t${CMSG}19${CEND}. Install yasd_xdebug(PHP>=7.2)"
-      read -e -p "Please input numbers:(Default '4 6 11 12' press Enter) " phpext_option
-      phpext_option=${phpext_option:-'4 6 11 12'}
+      echo -e "\t${CMSG}15${CEND}. Install pgsql"
+      echo -e "\t${CMSG}16${CEND}. Install swoole"
+      echo -e "\t${CMSG}17${CEND}. Install event(PHP>=5.4)"
+      echo -e "\t${CMSG}18${CEND}. Install grpc(PHP>=7.0)"
+      echo -e "\t${CMSG}19${CEND}. Install xdebug(PHP>=5.5)"
+      echo -e "\t${CMSG}20${CEND}. Install yasd(PHP>=7.2)"
+      read -e -p "Please input numbers:(Default '4 6 11 12 16 17' press Enter) " phpext_option
+      phpext_option=${phpext_option:-'4 6 11 12 16 17'}
       [ "${phpext_option}" == '0' ] && break
       array_phpext=(${phpext_option})
-      array_all=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19)
+      array_all=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
       for v in ${array_phpext[@]}
       do
       [ -z "`echo ${array_all[@]} | grep -w ${v}`" ] && phpext_flag=1
@@ -779,11 +780,12 @@ if [ ${ARG_NUM} == 0 ]; then
       [ -n "`echo ${array_phpext[@]} | grep -w 12`" ] && pecl_memcached=1
       [ -n "`echo ${array_phpext[@]} | grep -w 13`" ] && pecl_memcache=1
       [ -n "`echo ${array_phpext[@]} | grep -w 14`" ] && pecl_mongodb=1
-      [ -n "`echo ${array_phpext[@]} | grep -w 15`" ] && pecl_swoole=1
-      [ -n "`echo ${array_phpext[@]} | grep -w 16`" ] && pecl_event=1
-      [ -n "`echo ${array_phpext[@]} | grep -w 17`" ] && pecl_grpc=1
-      [ -n "`echo ${array_phpext[@]} | grep -w 18`" ] && pecl_xdebug=1
-      [ -n "`echo ${array_phpext[@]} | grep -w 19`" ] && yasd_xdebug=1
+      [ -n "`echo ${array_phpext[@]} | grep -w 15`" ] && pecl_pgsql=1
+      [ -n "`echo ${array_phpext[@]} | grep -w 16`" ] && pecl_swoole=1
+      [ -n "`echo ${array_phpext[@]} | grep -w 17`" ] && pecl_event=1
+      [ -n "`echo ${array_phpext[@]} | grep -w 18`" ] && pecl_grpc=1
+      [ -n "`echo ${array_phpext[@]} | grep -w 19`" ] && pecl_xdebug=1
+      [ -n "`echo ${array_phpext[@]} | grep -w 20`" ] && yasd=1
       break
       fi
   done
@@ -1289,7 +1291,7 @@ PHP_addons() {
   fi
 
   # pecl_pgsql
-  if [ -e "${pgsql_install_dir}/bin/psql" ]; then
+  if [ "${pecl_pgsql}" == '1' ]; then
     . include/language/php/extension/pecl_pgsql.sh
     Install_pecl_pgsql 2>&1 | tee -a ${oneinstack_dir}/install.log
   fi
