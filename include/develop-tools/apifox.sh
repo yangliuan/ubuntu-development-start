33601fe9b7ad7249d3825dc2291da3c96b253e2f
yangliuan <yangliuancn@foxmail.com>
2022-08-22 21:44:12 +0800

    md

--- include/develop-tools/apifox.sh
+++ include/develop-tools/apifox.sh
@@ -2,11 +2,13 @@
 Install_Apifox() {
     pushd ${oneinstack_dir}/src > /dev/null
     src_url='https://cdn.apifox.cn/download/Apifox-linux-manual-latest.tar.gz' && Download_src
-    tar -zxvf Apifox-linux-manual-latest.tar.gz
-    mv -fv apifox-${apifox_ver} /opt/apifox
+    [ ! -d apifox ] && mkdir apifox
+    #去掉第一层目录结构 DOC:https://wangchujiang.com/linux-command/c/tar.html
+    tar -zxvf Apifox-linux-manual-latest.tar.gz --strip-components 1 -C apifox
+    mv -fv apifox /opt/
     chown -R ${run_user}.${run_user} /opt/apifox
     chmod -R 775 /opt/apifox
-    rm -rfv apifox-${apifox_ver} Apifox-linux-manual-latest.tar.gz
+    rm -rfv apifox Apifox-linux-manual-latest.tar.gz
     cp -rfv ${oneinstack_dir}/desktop/apifox.desktop /usr/share/applications/
     
     popd > /dev/null
