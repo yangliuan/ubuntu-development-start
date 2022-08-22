94da2ff4588aa151ce8098177826de20a306f05b
yangliuan <yangliuancn@foxmail.com>
2022-08-22 21:59:20 +0800

    support ubuntu22.04

--- include/develop-tools/virtualbox.sh
+++ include/develop-tools/virtualbox.sh
@@ -2,10 +2,15 @@
 Install_Vbox() {
     pushd ${oneinstack_dir}/src > /dev/null
     echo "Download virtualbox ..."
-    src_url="https://download.virtualbox.org/virtualbox/${virtualbox_ver_base}.${virtualbox_ver}/virtualbox-${virtualbox_ver_base}_${virtualbox_ver_base}.${virtualbox_ver}-${virtualbox_ver_sn}~Ubuntu~eoan_amd64.deb" && Download_src
-    dpkg -i virtualbox-${virtualbox_ver_base}_${virtualbox_ver_base}.${virtualbox_ver}-${virtualbox_ver_sn}~Ubuntu~eoan_amd64.deb
+    if [ "${Ubuntu_ver}" == "22" ]; then
+        ubuntu_name="jammy"
+    else
+        ubuntu_name="eoan"
+    fi
+    src_url="https://download.virtualbox.org/virtualbox/${virtualbox_ver_base}.${virtualbox_ver}/virtualbox-${virtualbox_ver_base}_${virtualbox_ver_base}.${virtualbox_ver}-${virtualbox_ver_sn}~Ubuntu~${ubuntu_name}_amd64.deb" && Download_src
+    dpkg -i virtualbox-${virtualbox_ver_base}_${virtualbox_ver_base}.${virtualbox_ver}-${virtualbox_ver_sn}~Ubuntu~${ubuntu_name}_amd64.deb
     apt install -f
-    rm -rfv virtualbox-${virtualbox_ver_base}_${virtualbox_ver_base}.${virtualbox_ver}-${virtualbox_ver_sn}~Ubuntu~eoan_amd64.deb
+    rm -rfv virtualbox-${virtualbox_ver_base}_${virtualbox_ver_base}.${virtualbox_ver}-${virtualbox_ver_sn}~Ubuntu~${ubuntu_name}_amd64.deb
     src_url="https://download.virtualbox.org/virtualbox/${virtualbox_ver_base}.${virtualbox_ver}/Oracle_VM_VirtualBox_Extension_Pack-${virtualbox_ver_base}.${virtualbox_ver}.vbox-extpack" && Download_src
     mv -fv Oracle_VM_VirtualBox_Extension_Pack-${virtualbox_ver_base}.${virtualbox_ver}.vbox-extpack /home/${run_user}/Downloads/
     popd > /dev/null
