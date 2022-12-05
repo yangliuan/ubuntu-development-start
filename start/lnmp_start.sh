a0432a1c19c6847d34ac4751465c09bd25e36af0
yangliuan <yangliuancn@foxmail.com>
2022-12-05 17:04:10 +0800

    md

--- start/lnmp_start.sh
+++ start/lnmp_start.sh
@@ -10,3 +10,4 @@ printf "
 sudo systemctl start nginx.service
 sudo systemctl start php-fpm.service
 sudo systemctl start mysqld.service
+sudo systemctl status nginx.service php-fpm.service mysqld.service
