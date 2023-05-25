0ef51eccd8215cfff655f13b96273f8ca5de1639
yangliuan <yangliuancn@foxmail.com>
2023-05-25 09:27:32 +0800

    update runapi

--- develop-tools/api-test/runapi.sh
+++ develop-tools/api-test/runapi.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 Install_Runapi() {
     pushd ${oneinstack_dir}/src > /dev/null
-    echo "Download runapi..."    
-    src_url="https://www.showdoc.cc/server/api/common/visitfile/sign/0e73b106b92346fcd7809de0ee71f0ff?showdoc=.zip" && Download_src
-    unzip 0e73b106b92346fcd7809de0ee71f0ff\?showdoc\=.zip -d runapi
-    mkdir /opt/runapi
-    mv -v runapi/runapi.AppImage /opt/runapi/
-    rm -rfv runapi
-    cp -rfv ${oneinstack_dir}/icon/runapi.png /opt/runapi/
-    chown -Rv ${run_user}.${run_group} /opt/runapi/
-    cp -rfv ${oneinstack_dir}/desktop/runapi.desktop /usr/share/applications/
-    chown -Rv ${run_user}.${run_group} /usr/share/applications/runapi.desktop
+    echo "Download runapi..."
+    if [ ! -e "runapi.deb" ]; then
+        src_url="https://www.showdoc.com.cn/server/api/attachment/visitFile?sign=e0fb3d738511c03fafd73ef9e484a0d4" && Download_src
+        mv visitFile?sign=e0fb3d738511c03fafd73ef9e484a0d4 runapi.deb
+    fi
+    dpkg -i runapi.deb
+    [ -d "/opt/runapi" ] && chown -R ${run_user}.${run_user} /opt/runapi
     popd > /dev/null
 }
 
 Uninstall_Runapi() {
-    rm -rfv /opt/runapi/
-    rm -rfv /usr/share/applications/runapi.desktop
+    dpkg -P runapi
 }
