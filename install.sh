047414e3bb78fc5e74b3ee2e19b7d5650ba31b6e
yangliuan <yangliuancn@foxmail.com>
2023-06-08 21:40:16 +0800

    modify

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 custom_dir=$(dirname "`readlink -f $0`")
 
+# install ubsoft use costom agruments
+bash ${custom_dir}/setup/install_ubsoft.sh --input_method_option 2 --chrome --dingtalk --linuxqq --feishu --flameshot --indicator_sysmonitor --neteasy_cloudmusic --qqmusic --peek --qv2ray --theme_tools --bilibili_video_downloader --wps --custom --fceux
+
 # install devbase use costom agruments
 bash ${custom_dir}/setup/install_devbase.sh --nginx_option 1  --apache --apache_mode_option 1 --apache_mpm_option 1 --php_option 12 --phpcache_option 1 --php_extensions imagickfileinforedismemcachedeventpARallelrdkafka --jdk_option 2 --db_option 1 --dbrootpwd 123456 --dbinstallmethod 1 --elastic_stack --pureftpd --redis --memcached --mq_option 1 --nodejs_method 2 --python_option 2 --go_option 1 --ffmpeg --docker
 
@@ -10,5 +13,5 @@ bash ${custom_dir}/setup/install_devtools.sh --openssh_server --switchhost --rdm
 # install devaddons use costom agruments
 bash ${custom_dir}/setup/devaddons.sh --install --composer --supervisord --phpmyadmin
 
-# install ubsoft use costom agruments
-bash ${custom_dir}/setup/install_ubsoft.sh --input_method_option 2 --chrome --dingtalk --linuxqq --feishu --flameshot --indicator_sysmonitor --neteasy_cloudmusic --qqmusic --peek --qv2ray --theme_tools --bilibili_video_downloader --wps --custom --fceux --deepinwine
+# install wechat im
+bash ${custom_dir}/setup/install_ubsoft.sh --deepinwine
