f09df504ea9d56ba2e28c9315edb740ac62e793f
yangliuan <yangliuancn@foxmail.com>
2024-04-05 13:53:49 +0800

    edit download vscode

--- devtools/ide-editer/vscode.sh
+++ devtools/ide-editer/vscode.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 Install_Vscode() {
-    url="https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+    echo "Download vscode ..." 
+    src_url="https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
     #发送cURL请求，检索下载链接的最终URL
-    src_url=$(curl -sIL -o /dev/null -w '%{url_effective}' "$url")
-    echo "$src_url"
-    #将下载链接替换成中文cdn
-    src_url="http://vscode.cdn.azure.cn/stable/${src_url#*stable/}"
-    file_name="code_${src_url#*code_}"
+    final_url=$(curl -sIL -o /dev/null -w '%{url_effective}' "$src_url")
+    echo "$final_url"
     pushd ${ubdevenv_dir}/src > /dev/null
-    echo "Download vscode ..." && Download_src
+    Download_src
+    file_name="code_${final_url#*code_}"
+    mv "download?build=stable&os=linux-deb-x64" $file_name
     dpkg -i ${file_name}
-    #rm -rfv ${file_name}
+    rm -rfv ${file_name}
     popd > /dev/null
 }
 
