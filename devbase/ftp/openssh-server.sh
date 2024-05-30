30eaa0a9c99958a65917edcd34685499b8c08b97
yangliuan <yangliuancn@gmail.com>
2024-05-30 13:36:30 +0800

    md

--- devbase/ftp/openssh-server.sh
+++ devbase/ftp/openssh-server.sh
@@ -4,6 +4,7 @@ Install_OpensshServer(){
     #修改配置文件允许root远程登录
     sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
     systemctl disable ssh.service
+    systemctl disable ssh.socket
     systemctl start ssh.service
 }
 
