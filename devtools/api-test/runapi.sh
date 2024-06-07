f1297f73d607702f05375c060f5b397776585d19
yangliuan <yangliuancn@gmail.com>
2024-06-07 19:46:57 +0800

    md

--- devtools/api-test/runapi.sh
+++ devtools/api-test/runapi.sh
@@ -4,7 +4,7 @@ Install_Runapi() {
     echo "Download runapi..."
     if [ ! -e "runapi.deb" ]; then
         src_url="https://www.showdoc.com.cn/server/api/attachment/visitFile?sign=e0fb3d738511c03fafd73ef9e484a0d4" && Download_src
-        mv visitFile?sign=e0fb3d738511c03fafd73ef9e484a0d4 runapi.deb
+        mv visitFile runapi.deb
     fi
     dpkg -i runapi.deb
     [ -d "/opt/runapi" ] && chown -R ${run_user}:${run_group} /opt/runapi
